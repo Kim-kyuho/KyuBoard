afe1abcdf8c4eca4e6934cf29d2d1f89873bbdc6
Kim-kyuho <kgh9002@icloud.com>
2026-05-28 07:19:31 +0900

    delete forwardRef

--- components/ContextMenu.tsx
+++ components/ContextMenu.tsx
@@ -1,28 +1,32 @@
 "use client";
 
-import { forwardRef } from "react";
 import PressableButton from "./PressableButton";
 
 interface ContextMenuProps {
+    ref?: React.Ref<HTMLDivElement>;
     contextMenuPosition: { x: number; y: number };
     onDelete: () => void;
 
 }
-export default forwardRef<HTMLDivElement, ContextMenuProps>(
-  function ContextMenu({ contextMenuPosition, onDelete }, ref) {
-    return (
-      <div
-        ref={ref}
-        className="fixed bg-white px-3 py-4 shadow-md"
-        style={{
-          left: `${contextMenuPosition.x-90}px`,
-          top: `${contextMenuPosition.y-70}px`,
-        }}
-      >
-        <PressableButton variant="menu" onClick={onDelete}>
-          Delete
-        </PressableButton>
-      </div>
-    );
-  }
-);
+
+  export default function ContextMenu({ 
+  ref, 
+  contextMenuPosition, 
+  onDelete 
+}: ContextMenuProps) {
+  return (
+    <div
+      ref={ref}
+      className="fixed bg-white px-3 py-4 shadow-md"
+      style={{
+        left: `${contextMenuPosition.x-90}px`,
+        top: `${contextMenuPosition.y-70}px`,
+      }}
+    >
+      <PressableButton variant="menu" onClick={onDelete}>
+        Delete
+      </PressableButton>
+    </div>
+  );
+}
+
