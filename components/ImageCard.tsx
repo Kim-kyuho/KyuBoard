ce2ba81c1ae46ee94afad2661e7ae8162ffb308f
Kim-kyuho <105137901+Kim-kyuho@users.noreply.github.com>
2026-05-31 02:22:09 +0900

    ImageCard.tsx update

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Image from "next/image";
 import { Rnd } from "react-rnd";
 import ConfirmDialog from "@/components/ConfrimDialog";
@@ -53,7 +54,7 @@ export default function ImageCard(props: ImageCardProps) {
         onUpdate,
         onDelete,
     } = props;
-    // 이미지 카드의 상태와 핸들러를 관리하는 커스텀 훅을 사용하여 필요한 상태와 이벤트 핸들러들을 가져옴
+
     const {
         imageState,
         deleteDialogOpen,
@@ -87,7 +88,9 @@ export default function ImageCard(props: ImageCardProps) {
     return (
         <>
             <Rnd
-                className={`image-rnd-${image.imageId} select-none ${isSelected ? "rounded-xl border-2 border-dashed border-pink-500" : ""}`}
+                className={`image-rnd-${image.imageId} select-none ${
+                    isSelected ? "rounded-xl border-2 border-dashed border-pink-500" : ""
+                }`}
                 style={{
                     WebkitTouchCallout: "none",
                     WebkitUserSelect: "none",
@@ -112,19 +115,15 @@ export default function ImageCard(props: ImageCardProps) {
                 scale={zoom}
                 disableDragging={!isSelected}
                 enableResizing={isSelected}
-                /* 마우스 우클릭 이벤트 - 클릭한 좌표에 컨텍스트 메뉴를 표시 */
                 onContextMenu={handleContextMenu}
-                /* 모바일에서 길게 누름 이벤트 - 터치한 좌표에 컨텍스트 메뉴를 표시 */
                 onPointerDown={handleLongPressStart}
                 onPointerUp={clearLongPress}
                 onPointerMove={clearLongPress}
-                // 이미지 카드 이동 완료 시 좌표만 임시 저장
                 onDragStop={handleDragStop}
-                // 이미지 카드 크기 조절 완료 시 사이즈 정보만 임시 저장
                 onResizeStop={handleResizeStop}
             >
                 <div
-                    className="h-full w-full overflow-hidden rounded-xl bg-white shadow-md"
+                    className="relative h-full w-full overflow-hidden rounded-xl bg-white shadow-md"
                     onClick={handleImagePress}
                     onDoubleClick={selectImage}
                     onPointerDown={handleDoubleTap}
@@ -132,13 +131,14 @@ export default function ImageCard(props: ImageCardProps) {
                     <Image
                         src={image.secureUrl}
                         alt={image.fileName ?? "Uploaded image"}
+                        fill
                         draggable={false}
-                        className="h-full w-full object-contain"
+                        sizes={`${Math.round(imageState.width)}px`}
+                        className="object-contain"
                     />
                 </div>
             </Rnd>
 
-            {/* 컨텍스트 메뉴: Delete 버튼이 있는 메뉴 - 이미지 카드에서 우클릭 시 열림 */}
             {contextMenuOpen && (
                 <ContextMenu
                     ref={menuRef}
@@ -147,7 +147,6 @@ export default function ImageCard(props: ImageCardProps) {
                 />
             )}
 
-            {/* 삭제 확인 다이얼로그 - Yes 클릭 시 이미지 삭제 */}
             {deleteDialogOpen && (
                 <ConfirmDialog
                     message="Delete this image?"
