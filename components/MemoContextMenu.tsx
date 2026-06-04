44b4f8019fd8fbd30b41c7da14c07a4cb13685d6
Kim-kyuho <kgh9002@icloud.com>
2026-06-05 00:41:53 +0900

    Modify contextMenu Position

--- components/MemoContextMenu.tsx
+++ components/MemoContextMenu.tsx
@@ -31,8 +31,8 @@ export default function MemoContextMenu({
             ref={ref}
             className="fixed z-50 rounded-md bg-white px-3.5 py-px shadow-md"
             style={{
-                left: `${contextMenuPosition.x - 90}px`,
-                top: `${contextMenuPosition.y - 70}px`,
+                left: `${contextMenuPosition.x}px`,
+                top: `${contextMenuPosition.y}px`,
             }}
         >
             {isEditing && (
